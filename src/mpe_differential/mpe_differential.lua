163d5bc3670e6ecbccf024def470b97cb27e362e
sprk <sprrk@pm.me>
2025-10-24 02:15:14 +0200

    Rework differential

--- src/mpe_differential/mpe_differential.lua
+++ src/mpe_differential/mpe_differential.lua
@@ -2,49 +2,29 @@ local AXLE_A = 0
 local AXLE_B = 1
 local DRIVESHAFT_A = 2
 local DRIVESHAFT_B = 3
-local SLIP_FACTOR_SLOT = 0
+local SETTINGS_SLOT = 0
+local DATA_OUTPUT_SLOT = 1
 
 local DRIVE_RATIO = 1.0
+local LR_BIAS = 0
+local SLIP_FACTOR = 0 -- (0=locked, 1=open)
+local BASE_MASS = 0
 
-local AXLE_A_MASS = 0
-local AXLE_B_MASS = 0
-local DS_MASS = 0
+local AXLE_A_MASS = 1
+local AXLE_B_MASS = 1
+local DRIVESHAFT_MASS = 1
+
+local MASS_SMOOTHING_FACTOR = 0.5 -- [0..1] low=smooth, high=responsive
+local SLIP_MASS_FACTOR = 1
+
+local initialized = false
+local axle_bridge = false
 
 local function clamp(v, mn, mx)
 	return v < mn and mn or v > mx and mx or v
 end
 
-local initialized = false
-local axle_bridge = false
-
-local function calibrateMass()
-	-- Apply identical test impulse to each axle, measure resulting velocity
-	local TEST_MOMENTUM = 1.0
-
-	-- TODO: Fix inertia calculation for when this component is used
-	--       as a center diff; in that case the axles aren't connected to
-	--       wheels but to other diffs, which leads to incorrect mass values.
-
-	-- Measure AXLE_A inertia
-	component.slotTorqueApplyMomentum(AXLE_A, 1000, 0) -- Reset to 0
-	local rps_a = component.slotTorqueApplyMomentum(AXLE_A, 1, TEST_MOMENTUM)
-	local mass_a = (rps_a and rps_a ~= 0) and (TEST_MOMENTUM / rps_a) or 1.0
-
-	-- Measure AXLE_B inertia
-	component.slotTorqueApplyMomentum(AXLE_B, 1000, 0) -- Reset to 0
-	local rps_b = component.slotTorqueApplyMomentum(AXLE_B, 1, TEST_MOMENTUM)
-	local mass_b = (rps_b and rps_b ~= 0) and (TEST_MOMENTUM / rps_b) or 1.0
-
-	-- Driveshaft mass = sum of both axles (equal force distribution)
-	local massDrive = mass_a + mass_b
-
-	-- Reset all to 0
-	component.slotTorqueApplyMomentum(AXLE_A, mass_a, 0)
-	component.slotTorqueApplyMomentum(AXLE_B, mass_b, 0)
-	component.slotTorqueApplyMomentum(DRIVESHAFT_A, massDrive, 0)
-	component.slotTorqueApplyMomentum(DRIVESHAFT_B, massDrive, 0)
-
-	-- Check connections
+local function init()
 	local driveshaft_a_connected = component.slotTorqueIsConnected(DRIVESHAFT_A)
 	local driveshaft_b_connected = component.slotTorqueIsConnected(DRIVESHAFT_B)
 	local axle_a_connected = component.slotTorqueIsConnected(AXLE_A)
@@ -66,13 +46,9 @@ local function calibrateMass()
 			and component.slotTorqueSetBridgeFactor(DRIVESHAFT_A, 1)
 	end
 
-	-- Only return success if all bridges succeeded
-	local success = axle_bridge_ok and driveshaft_bridge_ok
-	return mass_a, mass_b, massDrive, success
-end
-
-local function init()
-	AXLE_A_MASS, AXLE_B_MASS, DS_MASS, initialized = calibrateMass()
+	if axle_bridge_ok and driveshaft_bridge_ok then
+		initialized = true
+	end
 end
 
 function onTick()
@@ -81,46 +57,75 @@ function onTick()
 		return
 	end
 
-	-- Slip input (0=locked, 1=open)
-	local slip = clamp(component.getInputLogicSlotFloat(SLIP_FACTOR_SLOT), 0, 1)
+	-- Read settings from composite input
+	local composite, composite_ok = component.getInputLogicSlotComposite(SETTINGS_SLOT)
+	if composite_ok then
+		local floats = composite.float_values
+		SLIP_FACTOR = clamp(floats[1], 0, 1)
+		--DRIVE_RATIO = floats[2] or 1 -- TODO: Clamp to sane limits
+		MASS_SMOOTHING_FACTOR = clamp(floats[3], 0, 1)
+		SLIP_MASS_FACTOR = math.max(floats[4], 0)
+		LR_BIAS = clamp(floats[5], -1, 1) -- TODO: Implement bias for torque vectoring
+		BASE_MASS = math.max(floats[6], 0)
+	end
+
 	if axle_bridge then
-		component.slotTorqueSetBridgeFactor(AXLE_A, 1 - slip)
+		component.slotTorqueSetBridgeFactor(AXLE_A, 1 - SLIP_FACTOR)
 	end
 
 	local driveshaft_a_connected = component.slotTorqueIsConnected(DRIVESHAFT_A)
 	local driveshaft_b_connected = component.slotTorqueIsConnected(DRIVESHAFT_B)
 
-	local driveshaft_rps = 0
-	if driveshaft_a_connected then
-		driveshaft_rps = component.slotTorqueApplyMomentum(DRIVESHAFT_A, 0, 0)
-	elseif driveshaft_b_connected then
-		driveshaft_rps = component.slotTorqueApplyMomentum(DRIVESHAFT_B, 0, 0)
-	end
-
+	-- Read current RPS values
 	local axle_a_rps = component.slotTorqueApplyMomentum(AXLE_A, 0, 0)
 	local axle_b_rps = component.slotTorqueApplyMomentum(AXLE_B, 0, 0)
 
-	local target_sum = 2 * driveshaft_rps / DRIVE_RATIO
-	local error = target_sum - axle_a_rps - axle_b_rps
-	local accel = error * 0.5
+	-- Move driveshaft RPS towards average of the two axles
+	local driveshaft_target_rps = ((axle_a_rps + axle_b_rps) * 0.5) * DRIVE_RATIO
 
+	-- Apply driveshaft momentum
+	local driveshaft_rps = 0
+	if driveshaft_a_connected then
+		driveshaft_rps = component.slotTorqueApplyMomentum(
+			DRIVESHAFT_A,
+			BASE_MASS + AXLE_A_MASS + AXLE_B_MASS,
+			driveshaft_target_rps
+		)
+	elseif driveshaft_b_connected then
+		driveshaft_rps = component.slotTorqueApplyMomentum(
+			DRIVESHAFT_B,
+			BASE_MASS + AXLE_A_MASS + AXLE_B_MASS,
+			driveshaft_target_rps
+		)
+	end
+
+	-- Calculate axle RPS targets
+	local target_sum = (driveshaft_rps / DRIVE_RATIO)
+	local accel = target_sum - axle_a_rps * 0.5 - axle_b_rps * 0.5
 	local axle_a_target_rps = axle_a_rps + accel
 	local axle_b_target_rps = axle_b_rps + accel
 
-	-- Momentum conservation: driveshaft resists by losing momentum to axles
-	-- TODO: Check for accuracy and balancing compared to stock parts
-	--local momentumLost = AXLE_A_MASS * accel + AXLE_B_MASS * accel
-	--local newDs = DS_MASS ~= 0 and (wDs - (momentumLost / DS_MASS)) or wDs
+	-- Apply axle momentum
+	local rps_a = component.slotTorqueApplyMomentum(AXLE_A, BASE_MASS + DRIVESHAFT_MASS, axle_a_target_rps)
+	local rps_b = component.slotTorqueApplyMomentum(AXLE_B, BASE_MASS + DRIVESHAFT_MASS, axle_b_target_rps)
 
-	local rps_a = component.slotTorqueApplyMomentum(AXLE_A, AXLE_A_MASS, axle_a_target_rps)
-	local rps_b = component.slotTorqueApplyMomentum(AXLE_B, AXLE_B_MASS, axle_b_target_rps)
+	local slip_magnitude_driveshaft = math.abs(driveshaft_target_rps - driveshaft_rps)
+	local calculated_driveshaft_mass = slip_magnitude_driveshaft * SLIP_MASS_FACTOR
+	DRIVESHAFT_MASS = DRIVESHAFT_MASS * (1 - MASS_SMOOTHING_FACTOR) + calculated_driveshaft_mass * MASS_SMOOTHING_FACTOR
 
-	-- Move driveshaft RPS towards average of the two axles
-	local driveshaft_target_rps = ((rps_a + rps_b) / 2) * DRIVE_RATIO
+	local slip_magnitude_axle_a = math.abs(axle_a_target_rps - rps_a)
+	local calculated_axle_a_mass = slip_magnitude_axle_a * SLIP_MASS_FACTOR
+	AXLE_A_MASS = AXLE_A_MASS * (1 - MASS_SMOOTHING_FACTOR) + calculated_axle_a_mass * MASS_SMOOTHING_FACTOR
 
-	if driveshaft_a_connected then
-		component.slotTorqueApplyMomentum(DRIVESHAFT_A, DS_MASS, driveshaft_target_rps)
-	elseif driveshaft_b_connected then
-		component.slotTorqueApplyMomentum(DRIVESHAFT_B, DS_MASS, driveshaft_target_rps)
-	end
+	local slip_magnitude_axle_b = math.abs(axle_b_target_rps - rps_b)
+	local calculated_axle_b_mass = slip_magnitude_axle_b * SLIP_MASS_FACTOR
+	AXLE_B_MASS = AXLE_B_MASS * (1 - MASS_SMOOTHING_FACTOR) + calculated_axle_b_mass * MASS_SMOOTHING_FACTOR
+
+	component.setOutputLogicSlotComposite(DATA_OUTPUT_SLOT, {
+		float_values = {
+			[1] = driveshaft_rps,
+			[2] = rps_a,
+			[3] = rps_b,
+		},
+	})
 end
