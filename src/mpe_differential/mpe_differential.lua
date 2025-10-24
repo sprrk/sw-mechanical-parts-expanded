1d1795c545f1b29e8fd50c497fffe4fec867f67d
sprk <sprrk@pm.me>
2025-10-24 16:14:47 +0200

    Rename settings and vars for clarity

--- src/mpe_differential/mpe_differential.lua
+++ src/mpe_differential/mpe_differential.lua
@@ -8,14 +8,14 @@ local DATA_OUTPUT_SLOT = 1
 local DRIVE_RATIO = 1.0
 local LR_BIAS = 0
 local SLIP_FACTOR = 0 -- (0=locked, 1=open)
-local BASE_MASS = 0
+local BASE_INERTIA = 0
 
-local AXLE_A_MASS = 1
-local AXLE_B_MASS = 1
-local DRIVESHAFT_MASS = 1
+local AXLE_A_INERTIA = 1
+local AXLE_B_INERTIA = 1
+local DRIVESHAFT_INERTIA = 1
 
-local MASS_SMOOTHING_FACTOR = 0.5 -- [0..1] low=smooth, high=responsive
-local SLIP_MASS_FACTOR = 1
+local INERTIA_SMOOTHING_FACTOR = 0.5 -- [0..1] low=smooth, high=responsive
+local RPS_SLIP_ENGAGEMENT_FACTOR = 1
 
 local initialized = false
 local axle_bridge = false
@@ -63,10 +63,10 @@ function onTick()
 		local floats = composite.float_values
 		SLIP_FACTOR = clamp(floats[1], 0, 1)
 		DRIVE_RATIO = clamp(floats[2] or 1, 0.1, 10)
-		MASS_SMOOTHING_FACTOR = clamp(floats[3], 0, 1)
-		SLIP_MASS_FACTOR = math.max(floats[4], 0)
+		INERTIA_SMOOTHING_FACTOR = clamp(floats[3], 0, 1)
+		RPS_SLIP_ENGAGEMENT_FACTOR = math.max(floats[4], 0)
 		LR_BIAS = clamp(floats[5], -1, 1) -- TODO: Implement bias for torque vectoring
-		BASE_MASS = math.max(floats[6], 0)
+		BASE_INERTIA = math.max(floats[6], 0)
 	end
 
 	if axle_bridge then
@@ -88,13 +88,13 @@ function onTick()
 	if driveshaft_a_connected then
 		driveshaft_rps = component.slotTorqueApplyMomentum(
 			DRIVESHAFT_A,
-			(BASE_MASS + AXLE_A_MASS + AXLE_B_MASS) / DRIVE_RATIO,
+			(BASE_INERTIA + AXLE_A_INERTIA + AXLE_B_INERTIA) / DRIVE_RATIO,
 			driveshaft_target_rps
 		)
 	elseif driveshaft_b_connected then
 		driveshaft_rps = component.slotTorqueApplyMomentum(
 			DRIVESHAFT_B,
-			(BASE_MASS + AXLE_A_MASS + AXLE_B_MASS) / DRIVE_RATIO,
+			(BASE_INERTIA + AXLE_A_INERTIA + AXLE_B_INERTIA) / DRIVE_RATIO,
 			driveshaft_target_rps
 		)
 	end
@@ -107,21 +107,24 @@ function onTick()
 
 	-- Apply axle momentum
 	local rps_a =
-		component.slotTorqueApplyMomentum(AXLE_A, (BASE_MASS + DRIVESHAFT_MASS) * DRIVE_RATIO, axle_a_target_rps)
+		component.slotTorqueApplyMomentum(AXLE_A, (BASE_INERTIA + DRIVESHAFT_INERTIA) * DRIVE_RATIO, axle_a_target_rps)
 	local rps_b =
-		component.slotTorqueApplyMomentum(AXLE_B, (BASE_MASS + DRIVESHAFT_MASS) * DRIVE_RATIO, axle_b_target_rps)
+		component.slotTorqueApplyMomentum(AXLE_B, (BASE_INERTIA + DRIVESHAFT_INERTIA) * DRIVE_RATIO, axle_b_target_rps)
 
 	local slip_magnitude_driveshaft = math.abs(driveshaft_target_rps - driveshaft_rps)
-	local calculated_driveshaft_mass = slip_magnitude_driveshaft * SLIP_MASS_FACTOR
-	DRIVESHAFT_MASS = DRIVESHAFT_MASS * (1 - MASS_SMOOTHING_FACTOR) + calculated_driveshaft_mass * MASS_SMOOTHING_FACTOR
+	local calculated_driveshaft_inertia = slip_magnitude_driveshaft * RPS_SLIP_ENGAGEMENT_FACTOR
+	DRIVESHAFT_INERTIA = DRIVESHAFT_INERTIA * (1 - INERTIA_SMOOTHING_FACTOR)
+		+ calculated_driveshaft_inertia * INERTIA_SMOOTHING_FACTOR
 
 	local slip_magnitude_axle_a = math.abs(axle_a_target_rps - rps_a)
-	local calculated_axle_a_mass = slip_magnitude_axle_a * SLIP_MASS_FACTOR
-	AXLE_A_MASS = AXLE_A_MASS * (1 - MASS_SMOOTHING_FACTOR) + calculated_axle_a_mass * MASS_SMOOTHING_FACTOR
+	local calculated_axle_a_inertia = slip_magnitude_axle_a * RPS_SLIP_ENGAGEMENT_FACTOR
+	AXLE_A_INERTIA = AXLE_A_INERTIA * (1 - INERTIA_SMOOTHING_FACTOR)
+		+ calculated_axle_a_inertia * INERTIA_SMOOTHING_FACTOR
 
 	local slip_magnitude_axle_b = math.abs(axle_b_target_rps - rps_b)
-	local calculated_axle_b_mass = slip_magnitude_axle_b * SLIP_MASS_FACTOR
-	AXLE_B_MASS = AXLE_B_MASS * (1 - MASS_SMOOTHING_FACTOR) + calculated_axle_b_mass * MASS_SMOOTHING_FACTOR
+	local calculated_axle_b_inertia = slip_magnitude_axle_b * RPS_SLIP_ENGAGEMENT_FACTOR
+	AXLE_B_INERTIA = AXLE_B_INERTIA * (1 - INERTIA_SMOOTHING_FACTOR)
+		+ calculated_axle_b_inertia * INERTIA_SMOOTHING_FACTOR
 
 	component.setOutputLogicSlotComposite(DATA_OUTPUT_SLOT, {
 		float_values = {
