7ecb999b02f61b308fe4f9c8132450db6e083617
sprk <sprrk@pm.me>
2025-11-01 16:02:05 +0100

    Improve CVT

--- src/mpe_cvt/mpe_cvt.lua
+++ src/mpe_cvt/mpe_cvt.lua
@@ -4,20 +4,26 @@ local LOSS = 0.15
 
 local init = false
 
-function onTick(tick_time)
-    if not init then
-        a_ok = component.slotTorqueCreateBridge(0, 1)
-        b_ok = component.slotTorqueSetBridgeFactor(0, 1-LOSS)
-        if a_ok and b_ok then
-            init = true
-        else
-            return
-        end
-    end
+function onTick(_)
+	if not init then
+		local a_ok = component.slotTorqueCreateBridge(0, 1)
+		local b_ok = component.slotTorqueSetBridgeFactor(0, 1 - LOSS)
+		if a_ok and b_ok then
+			init = true
+		else
+			return
+		end
+	end
 
-    ratio, ratio_get_ok = component.getInputLogicSlotFloat(0)
-    if ratio_get_ok then
-        if ratio < RATIO_MIN then ratio = RATIO_MIN elseif ratio > RATIO_MAX then ratio = RATIO_MAX end
-        component.slotTorqueSetBridgeRatio(0, ratio)
-    end
+	local ratio, ratio_get_ok = component.getInputLogicSlotFloat(0)
+	if ratio_get_ok then
+		if ratio < RATIO_MIN then
+			ratio = RATIO_MIN
+		elseif ratio > RATIO_MAX then
+			ratio = RATIO_MAX
+		end
+		component.slotTorqueSetBridgeRatio(0, ratio)
+	else
+		component.slotTorqueSetBridgeRatio(0, 1)
+	end
 end
